2b96011d8a68583eb0afd95df37f045a25e3d0f9
Andreas Fink <andreas@andreasfink.xyz>
2021-02-24 11:26:05 +0100

    add new function to generate color each day

--- script.py
+++ script.py
@@ -1,17 +1,41 @@
 import sys, traceback
 import os
-
-import RPi.GPIO as GPIO
 import requests
-import time
+from random import randrange
+from datetime import datetime, timedelta
+from threading import Timer
 import threading
-import random
+import schedule
+import time
+import RPi.GPIO as GPIO
 
+url_power = 'http://open.hab/rest/items/Gang_Power'
+url_color = 'http://open.hab/rest/items/Gang_Color'
 PIN=23
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN,GPIO.IN)
 
+color = "null"
 state = "false"
+
+def switchLight(key):
+    global color, brightness, state, url_power, url_color
+    if key == "OFF":
+        r = requests.post(url_power, 'OFF')
+        state = "false"
+    else: 
+        now = datetime.now()
+        if now.hour > 7 and now.hour < 19:
+            brightness = 100
+        else:
+            brightness = 15
+        r=requests.post(url_color, "{},100,{}".format(color, brightness))
+        state = "true"
+
+def generateColor():
+    global color
+    color = randrange(360)
 
 class ResumableTimer:
     def __init__(self, timeout, callback, args):
@@ -29,27 +53,20 @@ class ResumableTimer:
         self.timer = threading.Timer(self.timeout, self.callback, self.args)
         self.timer.start()
 
-def callUrl(key):
-    global state
-    r=requests.post('http://open.hab/rest/items/Gang_Power', key)
-    if key == "OFF":
-        state = "false"
-    #print(key == "OFF")
-    #print(key)
+generateColor()
+schedule.every().day.at("0:00").do(generateColor)
 
-timer = ResumableTimer(20, callUrl, ("OFF",))
+timer = ResumableTimer(30, switchLight, ("OFF",))
 
 try:
     while True:
+        schedule.run_pending()
         i=GPIO.input(PIN)
         if i==0:
-            #print("nothing detected")
             time.sleep(0.5)
         if i==1:
-            #print("something detected") 
             if state == "false":
-                callUrl('ON')
-                state = "true"
+                switchLight('ON')
                 timer.start()
             timer.reset()
             time.sleep(0.1)
